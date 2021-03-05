1d6dc77302a5ea62c53f03155209715de24c2a7c
Tim <tim@tedwards.org>
2021-03-05 15:02:21 -0600

    Adding Attending Workshops

--- HackerRank.cpp
+++ HackerRank.cpp
@@ -1,60 +1,77 @@
 // HackerRank.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
+#include <ctime>
 #include <vector>
-#include <sstream>
 
-
-void variableSizedArrays() {
-	std::vector<std::vector<int>> a;
-	int i = 0, j = 0, aSize = 0;
-
-	std::cin >> i >> j;  //  i contains the number of arrays, j contains the number of operations 
-	for (int x = 0; x < i; x++) {
-		std::vector<int> k;
-		std::cin >> aSize;
-		for (int y = 0; y < aSize; y++) {
-			int ae = 0;
-			std::cin >> ae;
-			k.push_back(ae);
-		}
-		a.push_back(k);
-	}
-	for (int x = 0; x < j; x++) {
-		int o1 = 0, o2 = 0;
-		std::cin >> o1 >> o2;
-		std::cout << a[o1][o2] << std::endl;
-	}
-}
-
-class Complex
-{
-public:
-	int a, b;
-	Complex& operator +(Complex& rhs);
-
+struct Workshop {
+	time_t start;
+	time_t end;
+	int duration;
 };
-Complex& Complex::operator+(Complex& rhs)
-{
-	a += rhs.a;
-	b += rhs.b;
-	return *this;
+
+struct Available_Workshops {
+	int n;
+	std::vector<Workshop> myWorkshops;
+};
+
+Available_Workshops* initialize(int start_time[], int duration[], int n) {
+	//   Creates an Available_Workshops object and initializes its elements using the 
+	//   elements in the  and  parameters (both are of size ). Here,  and  are the 
+	//   respective start time and duration for the  workshop. This function must 
+	//   return a pointer to an Available_Workshops object.
+	Available_Workshops* arrAvailableWorkshops = new Available_Workshops;
+	arrAvailableWorkshops->n = n;
+	for (int i = 0; i < n; i++) {
+		Workshop tempWrkShp;
+		tempWrkShp.start = start_time[i];
+		tempWrkShp.end = start_time[i] + duration[i];
+		tempWrkShp.duration = duration[i];
+		arrAvailableWorkshops->myWorkshops.push_back(tempWrkShp);
+	}
+	return arrAvailableWorkshops;
 }
 
-std::ostream& operator << (std::ostream& os, Complex& c) {
-	return std::cout << c.a << "+i" << c.b;
+int CalculateMaxWorkshops(Available_Workshops* ptr) {
+	//   Returns the maximum number of workshops the student can attend—without 
+	//   overlap. The next workshop cannot be attended until the previous workshop ends.
+	int retval = 0; //  the number of workshops a person can attend
+
+	if (ptr->n <= 0)
+		return retval;
+
+	int prevTotalTime = ptr->myWorkshops[0].start + ptr->myWorkshops[0].duration;
+
+	//loop through the workshops
+	//  compare prevTotalTime to current start
+	//  if prevTotalTime < current start time, they can attend the workshop
+	for (int x = 1; x < ptr->n; x++) {
+		if (prevTotalTime < ptr->myWorkshops[x].start)
+			++retval;
+	}
+
+	return retval;
 }
-void OverloadOperators() {
 
+int main(int argc, char* argv[]) {
+	int n; // number of workshops
+	std::cin >> n;
+	// create arrays of unknown size n
+	int* start_time = new int[n];
+	int* duration = new int[n];
 
-}
-int main()
-{
-	std::cout << "Hello World!\n";
+	for (int i = 0; i < n; i++) {
+		std::cin >> start_time[i];
+	}
+	for (int i = 0; i < n; i++) {
+		std::cin >> duration[i];
+	}
 
-	//variableSizedArrays();
-	OverloadOperators();
+	Available_Workshops* ptr;
+	ptr = initialize(start_time, duration, n);
+	int tim = CalculateMaxWorkshops(ptr);
+	std::cout << tim << std::endl;
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
