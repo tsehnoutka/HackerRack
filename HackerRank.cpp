e4215208c30082976f48ed719a889d998399d796
TIm Edwards <tim@tedwards.org>
2021-03-07 11:07:12 -0600

    this *should* have worked

--- HackerRank.cpp
+++ HackerRank.cpp
@@ -2,18 +2,27 @@
 //
 #include <iostream>
 #include <ctime>
+#include <algorithm>
 #include <vector>
 
 struct Workshop {
-	time_t start;
-	time_t end;
+	int start;
+	int end;
 	int duration;
 };
 
 struct Available_Workshops {
-	int n;
+	int n=0;
 	std::vector<Workshop> myWorkshops;
 };
+struct compare_start_time
+{
+	inline bool operator() (const Workshop&  lhs, const Workshop&  rhs)
+	{
+		return (lhs.start < rhs.start);
+	}
+};
+
 
 Available_Workshops* initialize(int start_time[], int duration[], int n) {
 	//   Creates an Available_Workshops object and initializes its elements using the 
@@ -29,25 +38,27 @@ Available_Workshops* initialize(int start_time[], int duration[], int n) {
 		tempWrkShp.duration = duration[i];
 		arrAvailableWorkshops->myWorkshops.push_back(tempWrkShp);
 	}
+	std::sort(arrAvailableWorkshops->myWorkshops.begin(), arrAvailableWorkshops->myWorkshops.end(), compare_start_time());
 	return arrAvailableWorkshops;
 }
 
 int CalculateMaxWorkshops(Available_Workshops* ptr) {
 	//   Returns the maximum number of workshops the student can attend—without 
 	//   overlap. The next workshop cannot be attended until the previous workshop ends.
-	int retval = 0; //  the number of workshops a person can attend
+	int retval = 1; //  the number of workshops a person can attend
 
 	if (ptr->n <= 0)
-		return retval;
+		return 0;
 
-	int prevTotalTime = ptr->myWorkshops[0].start + ptr->myWorkshops[0].duration;
+	int prevEndTime = ptr->myWorkshops[0].end;
 
 	//loop through the workshops
 	//  compare prevTotalTime to current start
 	//  if prevTotalTime < current start time, they can attend the workshop
-	for (int x = 1; x < ptr->n; x++) {
-		if (prevTotalTime < ptr->myWorkshops[x].start)
+	for (int x = 1; x < ptr->n-1; x++) {
+		if (prevEndTime <= ptr->myWorkshops[x].start)
 			++retval;
+		prevEndTime = ptr->myWorkshops[x].end;
 	}
 
 	return retval;
