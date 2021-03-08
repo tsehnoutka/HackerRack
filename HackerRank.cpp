404efcd5cae9c6973b71ede69a19115b39fd1109
Tim <tim@tedwards.org>
2021-03-08 11:15:43 -0600

    Works but too slow

--- HackerRank.cpp
+++ HackerRank.cpp
@@ -3,69 +3,29 @@
 
 #include <iostream>
 #include <vector>
-#include <sstream>
+#include<set>
 
+int uniqueIntegers(std::vector<int> a) {
+std::set<int> s;
 
-void variableSizedArrays() {
-	std::vector<std::vector<int>> a;
-	int i = 0, j = 0, aSize = 0;
-
-	std::cin >> i >> j;  //  i contains the number of arrays, j contains the number of operations 
-	for (int x = 0; x < i; x++) {
-		std::vector<int> k;
-		std::cin >> aSize;
-		for (int y = 0; y < aSize; y++) {
-			int ae = 0;
-			std::cin >> ae;
-			k.push_back(ae);
-		}
-		a.push_back(k);
-	}
-	for (int x = 0; x < j; x++) {
-		int o1 = 0, o2 = 0;
-		std::cin >> o1 >> o2;
-		std::cout << a[o1][o2] << std::endl;
-	}
-}
-
-class Complex
-{
-public:
-	int a, b;
-	Complex& operator +(Complex& rhs);
-
-};
-Complex& Complex::operator+(Complex& rhs)
-{
-	a += rhs.a;
-	b += rhs.b;
-	return *this;
-}
-
-std::ostream& operator << (std::ostream& os, Complex& c) {
-	return std::cout << c.a << "+i" << c.b;
-}
-void OverloadOperators() {
-
-
+	for (int x : a)
+		s.insert(x);
+	return s.size();
 }
 int main()
 {
-	std::cout << "Hello World!\n";
+	int N = 0, S = 0, P = 0, Q = 0;
+	std::cin >> N >> S >> P >> Q;
+	std::vector<int> a(N);
 
-	//variableSizedArrays();
-	OverloadOperators();
+
+	a[0] = S % (2 ^ 31);
+	for (int i = 1; i <= N - 1; i++) {
+		a[i] = (a[i - 1] * P) +(Q % (2 ^ 31));
+	}
+	std::cout << uniqueIntegers(a);
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
 
 
---
+++ Problem.txt
@@ -0,0 +1,30 @@
+You are given four integers: , , , . You will use them in order to create the sequence  with the following pseudo-code.
+
+a[0] = S (modulo 2^31)
+for i = 1 to N-1
+    a[i] = a[i-1]*P+Q (modulo 2^31) 
+Your task is to calculate the number of distinct integers in the sequence .
+
+Input Format
+
+Four space separated integers on a single line, , , , and  respectively.
+
+Output Format
+
+A single integer that denotes the number of distinct integers in the sequence .
+
+Constraints
+
+
+
+
+Sample Input
+
+3 1 1 1
+Sample Output
+
+3
+Explanation
+
+
+Hence, there are  different integers in the sequence.
