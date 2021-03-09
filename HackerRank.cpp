d2a2ed64cd45929eb50c07891c69616ec31793a1
Tim <tim@tedwards.org>
2021-03-09 08:16:28 -0600

    string is working

--- HackerRank.cpp
+++ HackerRank.cpp
@@ -1,38 +1,21 @@
-#include <sstream>
-#include <vector>
+#include <string>
 #include <iostream>
-#include <algorithm>
-using namespace std;
 
-vector<int> parseInts(string str) {
-    // Complete this function
-    
-    vector<int> retVal;
-    char ch='0';
-    int t=0; 
-    stringstream words(str);
-
-    words >> t;
-    words >> ch;
-    int iCommaCount = std::count(str.begin(), str.end(), ',');
-    while(iCommaCount>=0)
-    {
-        retVal.push_back(t);
-        words >> t;
-        words >> ch;  //  get rid of comma
-        iCommaCount--;
-    }
-    
-    return retVal;
-}
 
 int main() {
-    string str;
-    cin >> str;
-    vector<int> integers = parseInts(str);
-    for (int i = 0; i < integers.size(); i++) {
-        cout << integers[i] << "\n";
-    }
+	std::string str1 = "";
+	std::string str2 = "";
 
-    return 0;
+	std::cin >> str1;
+	std::cin >> str2;
+
+	std::cout << str1.length() << " " << str2.length() << std::endl;
+	std::cout << str1 + str2 << std::endl;
+
+	char temp = str1[0];
+	str1[0] = str2[0];
+	str2[0] = temp;
+
+	std::cout << str1 << " " << str2 << std::endl;
+	return 0;
 }
