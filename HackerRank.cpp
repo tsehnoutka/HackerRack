d209be834b3847b760bc02b1ab0507031019bfdc
Tim <tim@tedwards.org>
2021-03-09 08:49:59 -0600

    classes and Objects works

--- HackerRank.cpp
+++ HackerRank.cpp
@@ -2,70 +2,55 @@
 //
 
 #include <iostream>
-#include <vector>
-#include <sstream>
-
-
-void variableSizedArrays() {
-	std::vector<std::vector<int>> a;
-	int i = 0, j = 0, aSize = 0;
-
-	std::cin >> i >> j;  //  i contains the number of arrays, j contains the number of operations 
-	for (int x = 0; x < i; x++) {
-		std::vector<int> k;
-		std::cin >> aSize;
-		for (int y = 0; y < aSize; y++) {
-			int ae = 0;
-			std::cin >> ae;
-			k.push_back(ae);
-		}
-		a.push_back(k);
-	}
-	for (int x = 0; x < j; x++) {
-		int o1 = 0, o2 = 0;
-		std::cin >> o1 >> o2;
-		std::cout << a[o1][o2] << std::endl;
-	}
-}
-
-class Complex
-{
+class Student {
+private:
+	static const int MAX_SCORES = 5;
+	int scores[MAX_SCORES];
 public:
-	int a, b;
-	Complex& operator +(Complex& rhs);
+	void input();
+	int calculateTotalScore();
 
 };
-Complex& Complex::operator+(Complex& rhs)
-{
-	a += rhs.a;
-	b += rhs.b;
-	return *this;
+
+void Student::input() {
+    for (int x = 0; x < MAX_SCORES; x++)
+        std::cin >> scores[x];
+}
+int Student::calculateTotalScore() {
+	int total = 0;
+	for (int x = 0; x < MAX_SCORES; x++)
+		total += scores[x];
+	return total;
 }
 
-std::ostream& operator << (std::ostream& os, Complex& c) {
-	return std::cout << c.a << "+i" << c.b;
-}
-void OverloadOperators() {
 
-
-}
 int main()
 {
-	std::cout << "Hello World!\n";
+    int n; // number of students
+    std::cin >> n;
+    Student* s = new Student[n]; // an array of n students
 
-	//variableSizedArrays();
-	OverloadOperators();
+    for (int i = 0; i < n; i++) {
+        s[i].input();
+    }
+
+    // calculate kristen's score
+    int kristen_score = s[0].calculateTotalScore();
+
+    // determine how many students scored higher than kristen
+    int count = 0;
+    for (int i = 1; i < n; i++) {
+        int total = s[i].calculateTotalScore();
+        if (total > kristen_score) {
+            count++;
+        }
+    }
+
+    // print result
+    std::cout << count;
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
 
 
