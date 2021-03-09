65f53d748358c8a3b26fa7db38edbb0f5c9fa1c7
Tim <tim@tedwards.org>
2021-03-09 09:51:19 -0600

    Box it! works

--- HackerRank.cpp
+++ HackerRank.cpp
@@ -1,71 +1,85 @@
 // HackerRank.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
-#include <vector>
-#include <sstream>
 
-
-void variableSizedArrays() {
-	std::vector<std::vector<int>> a;
-	int i = 0, j = 0, aSize = 0;
-
-	std::cin >> i >> j;  //  i contains the number of arrays, j contains the number of operations 
-	for (int x = 0; x < i; x++) {
-		std::vector<int> k;
-		std::cin >> aSize;
-		for (int y = 0; y < aSize; y++) {
-			int ae = 0;
-			std::cin >> ae;
-			k.push_back(ae);
-		}
-		a.push_back(k);
-	}
-	for (int x = 0; x < j; x++) {
-		int o1 = 0, o2 = 0;
-		std::cin >> o1 >> o2;
-		std::cout << a[o1][o2] << std::endl;
-	}
-}
-
-class Complex
-{
+class Box {
+private:
+	int length;
+	int bredth;
+	int height;
 public:
-	int a, b;
-	Complex& operator +(Complex& rhs);
+	Box() : length(0), bredth(0), height(0) {}
+	Box(int l, int b, int h) : length(l), bredth(b), height(h) {}
+	Box(const Box& b) :length(b.length), bredth(b.bredth), height(b.height) {}
 
+	int getLength() { return length; }
+	int getBreadth() { return bredth; }
+	int getHeight() { return height; }
+	long long CalculateVolume() {
+		long long volume = length * bredth * height;
+		return  volume;
+	}
+
+	friend bool  operator<(const Box& lhs, const Box& rhs) {
+		return (lhs.length < rhs.length) ||
+			(lhs.bredth < rhs.bredth&& lhs.length == rhs.length) ||
+			(lhs.height < rhs.height&& lhs.length == rhs.length && lhs.bredth == rhs.bredth);
+	}
+	friend std::ostream& operator<<(std::ostream& os, const Box& b) {
+		os << b.length << " " << b.bredth << " " << b.height;
+		return os;
+	}
 };
-Complex& Complex::operator+(Complex& rhs)
+
+void check2()
 {
-	a += rhs.a;
-	b += rhs.b;
-	return *this;
+	int n;
+	std::cin >> n;
+	Box temp;
+	for (int i = 0; i < n; i++)
+	{
+		int type;
+		std::cin >> type;
+		if (type == 1)
+		{
+			std::cout << temp << std::endl;
+		}
+		if (type == 2)
+		{
+			int l, b, h;
+			std::cin >> l >> b >> h;
+			Box NewBox(l, b, h);
+			temp = NewBox;
+			std::cout << temp << std::endl;
+		}
+		if (type == 3)
+		{
+			int l, b, h;
+			std::cin >> l >> b >> h;
+			Box NewBox(l, b, h);
+			if (NewBox < temp)
+			{
+				std::cout << "Lesser\n";
+			}
+			else
+			{
+				std::cout << "Greater\n";
+			}
+		}
+		if (type == 4)
+		{
+			std::cout << temp.CalculateVolume() << std::endl;
+		}
+		if (type == 5)
+		{
+			Box NewBox(temp);
+			std::cout << NewBox << std::endl;
+		}
+
+	}
 }
 
-std::ostream& operator << (std::ostream& os, Complex& c) {
-	return std::cout << c.a << "+i" << c.b;
-}
-void OverloadOperators() {
-
-
-}
 int main()
 {
-	std::cout << "Hello World!\n";
-
-	//variableSizedArrays();
-	OverloadOperators();
+	check2();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-
-
