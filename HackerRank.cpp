0b698574c47f5c635f995dbf23391a6b0ede88aa
Tim <tim@tedwards.org>
2021-03-11 17:57:09 -0600

    works but TOO SLOW

--- HackerRank.cpp
+++ HackerRank.cpp
@@ -7,23 +7,39 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    int count = 0;
-    vector<int> v;
-    cin >> count;
-    for (int x = 0; x < count; x++) {
-        int item;
-        cin >> item;
-        v.push_back(item);
-    }
-    int index = 0, begin = 0, end = 0;
-    cin >> index >> begin >> end;
-    v.erase(v.begin()+index - 1); // subtract one because input is for a 1 based array ( not a zero based array )
-    v.erase(v.begin() + begin-1, v.begin() + end-1);  // subtract one because input is for a 1 based array ( not a zero based array )
-    cout << v.size() << endl;;
-    for (int x : v) {
-        cout << x << " ";
-    }
-    cout << endl;
-    return 0;
-}
+	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+	int count = 0;
+	vector<int> v;
+
+	std::cin >> count;
+	for (int x = 0; x < count; x++) {
+		int item;
+		std::cin >> item;
+		v.push_back(item);
+	}
+	sort(v.begin(), v.end());
+
+	int q;
+	std::cin >> q;
+	for (int x = 0; x < q; x++) {  //  perform the equeries
+		int y;
+		std::cin >> y;
+		unsigned int i = 0;
+		bool found = false;
+		while ( (!found) && (y >= v[i]) && (i < v.size()) ) {
+			if (v[i] == y) {
+				std::cout << "Yes " << i+1 << std::endl; //  need to add one to index casue thi user is a 1-based array
+				found = true;
+			}
+			else {
+				++i;
+
+			}//  end else
+		}  //  end while
+		if (!found)
+			std::cout << "No " << i+1 << std::endl;//  need to add one to index casue thi user is a 1-based array
+	}  // end for
+
+return 0;
+}  //  end main
+
