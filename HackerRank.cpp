b7fddb8b71a742acae87dc849ec5d1f7029ebe95
Tim <tim@tedwards.org>
2021-03-12 11:11:28 -0600

    Storing what I have

--- HackerRank.cpp
+++ HackerRank.cpp
@@ -1,71 +1,35 @@
-// HackerRank.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-#include <vector>
-#include <sstream>
+#include <algorithm>    // std::sort
+#include <deque> 
+using namespace std;
 
-
-void variableSizedArrays() {
-	std::vector<std::vector<int>> a;
-	int i = 0, j = 0, aSize = 0;
-
-	std::cin >> i >> j;  //  i contains the number of arrays, j contains the number of operations 
-	for (int x = 0; x < i; x++) {
-		std::vector<int> k;
-		std::cin >> aSize;
-		for (int y = 0; y < aSize; y++) {
-			int ae = 0;
-			std::cin >> ae;
-			k.push_back(ae);
-		}
-		a.push_back(k);
-	}
-	for (int x = 0; x < j; x++) {
-		int o1 = 0, o2 = 0;
-		std::cin >> o1 >> o2;
-		std::cout << a[o1][o2] << std::endl;
-	}
-}
-
-class Complex
-{
-public:
-	int a, b;
-	Complex& operator +(Complex& rhs);
-
-};
-Complex& Complex::operator+(Complex& rhs)
-{
-	a += rhs.a;
-	b += rhs.b;
-	return *this;
-}
-
-std::ostream& operator << (std::ostream& os, Complex& c) {
-	return std::cout << c.a << "+i" << c.b;
-}
-void OverloadOperators() {
-
+void printKMax(int arr[], int n, int k) {
+    //Write your code here.
+    std::deque<int> mydeque(arr, arr + sizeof(arr) / sizeof(int));
+    deque<int>::iterator it = mydeque.begin();
+    int iterations = n - k + 1;
+    while (iterations > 0) {
+        int max = *max_element(it, it + k);
+        cout << max << " ";
+        it++;
+        iterations--;
+    }
+    cout << endl;
 
 }
-int main()
-{
-	std::cout << "Hello World!\n";
+int main() {
 
-	//variableSizedArrays();
-	OverloadOperators();
+    int t=1;
+    cin >> t;
+    while (t > 0) {
+        int n=5, k=2;
+        cin >> n >> k;
+        int i;
+        int arr[n];
+        for (i = 0; i < n; i++)
+            cin >> arr[i];
+        printKMax(arr, n, k);
+        t--;
+    }
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-
-
