5e427d7e39ca3bcd5694cccccb4f072e82dd7aab
Tim <tim@tedwards.org>
2021-03-12 09:03:42 -0600

    Map STL works

--- HackerRank.cpp
+++ HackerRank.cpp
@@ -1,71 +1,45 @@
-// HackerRank.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+#include <cmath>
+#include <cstdio>
 #include <vector>
-#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
 
 
-void variableSizedArrays() {
-	std::vector<std::vector<int>> a;
-	int i = 0, j = 0, aSize = 0;
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    map<string, int>m;
+    map<string, int>::iterator it;
+    int q = 0, x=0,y=0;
+    string name = "";
+    cin >> q;
 
-	std::cin >> i >> j;  //  i contains the number of arrays, j contains the number of operations 
-	for (int x = 0; x < i; x++) {
-		std::vector<int> k;
-		std::cin >> aSize;
-		for (int y = 0; y < aSize; y++) {
-			int ae = 0;
-			std::cin >> ae;
-			k.push_back(ae);
-		}
-		a.push_back(k);
-	}
-	for (int x = 0; x < j; x++) {
-		int o1 = 0, o2 = 0;
-		std::cin >> o1 >> o2;
-		std::cout << a[o1][o2] << std::endl;
-	}
+    for (int i = 0; i < q; i++) {
+        cin >> x ;
+        switch (x) {
+        case 1:
+            cin >> name >> y;
+             it = m.find(name);
+             if (it == m.end()) // not found
+                m.insert(make_pair(name, y));
+             else {
+                 m[name] = m[name] + y;
+             }
+            break;
+        case 2:
+            cin >> name ;
+            m.erase(name);
+            break;
+        case 3:
+            cin >> name ;
+            it = m.find(name);
+            if (it == m.end())
+                cout << "0\n";
+            else
+                cout << m[name] << endl;
+            break;
+        }
+    }
+    return 0;
 }
-
-class Complex
-{
-public:
-	int a, b;
-	Complex& operator +(Complex& rhs);
-
-};
-Complex& Complex::operator+(Complex& rhs)
-{
-	a += rhs.a;
-	b += rhs.b;
-	return *this;
-}
-
-std::ostream& operator << (std::ostream& os, Complex& c) {
-	return std::cout << c.a << "+i" << c.b;
-}
-void OverloadOperators() {
-
-
-}
-int main()
-{
-	std::cout << "Hello World!\n";
-
-	//variableSizedArrays();
-	OverloadOperators();
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-
-
