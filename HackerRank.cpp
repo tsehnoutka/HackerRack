5d644825a02c3e9585ac2f00294b09016e94f823
Tim <tim@tedwards.org>
2021-03-16 15:45:42 -0500

    works for simple case

--- HackerRank.cpp
+++ HackerRank.cpp
@@ -1,71 +1,95 @@
-// HackerRank.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+#include <cmath>
+#include <cstdio>
 #include <vector>
-#include <sstream>
+#include <iostream>
+#include<string>
+#include <algorithm>
+#include <map>
+#include <queue>
+using namespace std;
 
+// trim from start (in place)
+static inline void ltrim(std::string& s) {
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+		return !std::isspace(ch);
+		}));
+}
 
-void variableSizedArrays() {
-	std::vector<std::vector<int>> a;
-	int i = 0, j = 0, aSize = 0;
+// trim from end (in place)
+static inline void rtrim(std::string& s) {
+	s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+		return !std::isspace(ch);
+		}).base(), s.end());
+}
 
-	std::cin >> i >> j;  //  i contains the number of arrays, j contains the number of operations 
-	for (int x = 0; x < i; x++) {
-		std::vector<int> k;
-		std::cin >> aSize;
-		for (int y = 0; y < aSize; y++) {
-			int ae = 0;
-			std::cin >> ae;
-			k.push_back(ae);
+// trim from both ends (in place)
+static inline void trim(string& s) {
+	ltrim(s);
+	rtrim(s);
+}
+
+int main() {
+	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+	map <string, string> tagName;
+	queue<string> tags;
+	queue<string> createKey;
+	string line;
+
+	int n = 0, q = 0;
+	cin >> n >> q;
+	cin.ignore();
+
+	for (int x = 0; x < n; x++) {
+		getline(cin, line);
+
+		//parse line
+		if (line[1] == '/') {
+			//  closing tag
+			tags.pop();
 		}
-		a.push_back(k);
+		else {
+			//  Opening tag
+			int spacePos = line.find(' ');
+			string tag = line.substr(1, spacePos - 1);
+			tags.push(tag);
+			unsigned int numOfEqualSigns = std::count(line.begin(), line.end(), '=');
+			unsigned int equalSign = 0;
+			unsigned int first_quote = 0, second_quote = 0;
+
+			for (unsigned int x = 0; x < numOfEqualSigns; x++) {
+				equalSign = line.find('=', equalSign+1);
+				spacePos = line.rfind(' ', equalSign -2);
+				string key = line.substr(spacePos + 1, equalSign - spacePos - 2);  //  find the space between the tag and the kexy
+				//  need to update the variable space pos.........( or something......)
+				trim(key);
+				first_quote = line.find('\"',second_quote+1);
+				second_quote = line.find('\"', first_quote+1);
+				string value = line.substr(first_quote+1, second_quote-first_quote-1);
+				createKey = tags;
+				string mapKey = "";
+				while (!createKey.empty()) {
+					mapKey += createKey.front() + ".";
+					createKey.pop();
+				}
+				unsigned int strLengh = mapKey.length();
+				mapKey[strLengh - 1] = '~';
+				mapKey += key;
+				tagName.insert(pair<string, string>(mapKey,value));
+			}
+		}
+	} //  end of reading in lines
+
+	//  handle queries
+	map <string, string>::iterator it;
+	for (int x = 0; x < q; x++) {
+		getline(cin, line);
+		it = tagName.find(line);
+		if (it != tagName.end()) {
+			cout << it->second << endl;
+		}
+		else
+			cout << "Not Found!\n";
 	}
-	for (int x = 0; x < j; x++) {
-		int o1 = 0, o2 = 0;
-		std::cin >> o1 >> o2;
-		std::cout << a[o1][o2] << std::endl;
-	}
+
+	return 0;
 }
-
-class Complex
-{
-public:
-	int a, b;
-	Complex& operator +(Complex& rhs);
-
-};
-Complex& Complex::operator+(Complex& rhs)
-{
-	a += rhs.a;
-	b += rhs.b;
-	return *this;
-}
-
-std::ostream& operator << (std::ostream& os, Complex& c) {
-	return std::cout << c.a << "+i" << c.b;
-}
-void OverloadOperators() {
-
-
-}
-int main()
-{
-	std::cout << "Hello World!\n";
-
-	//variableSizedArrays();
-	OverloadOperators();
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-
-
