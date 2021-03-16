bd6e88d24ef30038a00121496c3f27249727bf01
Tim <tim@tedwards.org>
2021-03-16 15:58:51 -0500

    Someone else's solution

--- HackerRank.cpp
+++ HackerRank.cpp
@@ -8,95 +8,61 @@
 #include <queue>
 using namespace std;
 
-// trim from start (in place)
-static inline void ltrim(std::string& s) {
-	s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
-		return !std::isspace(ch);
-		}));
-}
+#include <iostream>
+#include <map>
+using namespace std;
 
-// trim from end (in place)
-static inline void rtrim(std::string& s) {
-	s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
-		return !std::isspace(ch);
-		}).base(), s.end());
-}
+map <string, string> tagMap;
 
-// trim from both ends (in place)
-static inline void trim(string& s) {
-	ltrim(s);
-	rtrim(s);
+void createMap(int& n, string pretag) {
+    if (!n) return;
+
+    string line, tag, attr, value;
+    getline(cin, line);
+
+    int i = 1;
+    if (line[i] == '/') {           // found closing tag
+        while (line[i] != '>') i++;
+        if (pretag.size() > (i - 2))		// update tag
+            tag = pretag.substr(0, pretag.size() - i + 1);
+        else
+            tag = "";
+    }
+    else {                       // found opening tag
+        while (line[i] != ' ' && line[i] != '>') i++;
+        tag = line.substr(1, i - 1);	// update tag
+        if (pretag != "") tag = pretag + "." + tag;
+
+        int j;
+        while (line[i] != '>') { // go through attributes
+            j = ++i;
+            while (line[i] != ' ') i++;
+            attr = line.substr(j, i - j);	// attribute name
+
+            while (line[i] != '\"') i++;
+            j = ++i;
+            while (line[i] != '\"') i++;
+            value = line.substr(j, i - j);	// attribute value
+            i += 1;
+
+            tagMap[tag + "~" + attr] = value;
+        }
+    }
+    createMap(--n, tag);
 }
 
 int main() {
-	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
-	map <string, string> tagName;
-	deque<string> tags;
-	deque<string> createKey;
-	string line;
+    int n, q;
+    cin >> n >> q;
+    cin.ignore();
+    createMap(n, "");
 
-	int n = 0, q = 0;
-	cin >> n >> q;
-	cin.ignore();
-
-	for (int x = 0; x < n; x++) {
-		getline(cin, line);
-
-		//parse line
-		if (line[1] == '/') {
-			//  closing tag
-			tags.pop_back();
-		}
-		else {
-			//  Opening tag
-			int spacePos = line.find(' ');
-			string tag = "";
-			if (spacePos == -1) {
-				tag = line.substr(1,line.size()-2);
-			}
-			else{
-				tag = line.substr(1, spacePos - 1);
-
-			}
-			tags.push_back(tag);
-			unsigned int numOfEqualSigns = std::count(line.begin(), line.end(), '=');
-			unsigned int equalSign = 0;
-			unsigned int first_quote = 0, second_quote = 0;
-
-			for (unsigned int x = 0; x < numOfEqualSigns; x++) {
-				equalSign = line.find('=', equalSign+1);
-				spacePos = line.rfind(' ', equalSign -2);
-				string key = line.substr(spacePos + 1, equalSign - spacePos - 2);  //  find the space between the tag and the kexy
-				//  need to update the variable space pos.........( or something......)
-				trim(key);
-				first_quote = line.find('\"',second_quote+1);
-				second_quote = line.find('\"', first_quote+1);
-				string value = line.substr(first_quote+1, second_quote-first_quote-1);
-				createKey = tags;
-				string mapKey = "";
-				while (!createKey.empty()) {
-					mapKey += createKey.front() + ".";
-					createKey.pop_front();
-				}
-				unsigned int strLengh = mapKey.length();
-				mapKey[strLengh - 1] = '~';
-				mapKey += key;
-				tagName.insert(pair<string, string>(mapKey,value));
-			}
-		}
-	} //  end of reading in lines
-
-	//  handle queries
-	map <string, string>::iterator it;
-	for (int x = 0; x < q; x++) {
-		getline(cin, line);
-		it = tagName.find(line);
-		if (it != tagName.end()) {
-			cout << it->second << endl;
-		}
-		else
-			cout << "Not Found!\n";
-	}
-
-	return 0;
+    string attr, value;
+    while (q--) {
+        getline(cin, attr);
+        value = tagMap[attr];
+        if (value == "") value = "Not Found!";
+        cout << value << endl;
+    }
+    return 0;
 }
